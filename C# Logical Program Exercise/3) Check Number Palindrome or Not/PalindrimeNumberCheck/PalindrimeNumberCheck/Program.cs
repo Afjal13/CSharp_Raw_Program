4c3ade5c8e6f49548aa8f12b7683c95f9c5c871c
Afjal13 <afjal.hossain12496@gmail.com>
2023-07-14 12:00:05 +0600

    07-14-2023

---
+++ C# Logical Program Exercise/3) Check Number Palindrome or Not/PalindrimeNumberCheck/PalindrimeNumberCheck/Program.cs
@@ -0,0 +1,21 @@
+﻿//Required variable initialization
+int userInput, temp, divEndResult, sum = 0;
+//take a user input
+Console.Write("Enter a Number(digit):__");
+userInput = Convert.ToInt32(Console.ReadLine());
+//user input store to temp variable
+temp = userInput;
+//palindrome cookie
+while (userInput > 0)
+{
+    divEndResult = userInput % 10;
+    sum = (sum * 10) + divEndResult;
+    userInput = userInput / 10;
+}
+//Check given user number palindrome or not
+if (temp == sum)
+    Console.WriteLine($"{temp} is Palindrome!");
+else
+    Console.WriteLine($"{temp} is not Palindrome!");
+
+Console.ReadKey();
