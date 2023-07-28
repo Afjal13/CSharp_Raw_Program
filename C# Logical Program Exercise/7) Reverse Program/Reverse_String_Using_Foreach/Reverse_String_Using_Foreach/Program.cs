d9fd32495b6ef624a8d60bca0e4bd2caa4402c96
Afjal13 <afjal.hossain12496@gmail.com>
2023-07-29 00:10:41 +0600

    07-28-2023

---
+++ C# Logical Program Exercise/7) Reverse Program/Reverse_String_Using_Foreach/Reverse_String_Using_Foreach/Program.cs
@@ -0,0 +1,29 @@
+﻿//How to reverse a string using for each loop in C#
+
+//declare required variable fields
+string inputStr, reverseStr;
+
+//Take input String 
+Console.Write("\nEnter a String: ");
+inputStr = Console.ReadLine();
+
+//initialize by empty value
+reverseStr = string.Empty;
+
+//String reverse mechanism using foreach loop
+foreach (char c in inputStr)
+{
+    reverseStr = c + reverseStr;
+}
+
+//Display reverse string 
+Console.WriteLine($"\n\tReverse of given string is: {reverseStr}\n");
+
+//hold console window
+Console.ReadKey();
+
+//output
+
+//Enter a String: Afjal
+
+//        Reverse of given string is: lajfA
