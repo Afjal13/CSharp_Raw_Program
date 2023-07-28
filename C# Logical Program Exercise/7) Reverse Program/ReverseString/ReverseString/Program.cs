73587d5f1c769ba310cf33d8d56fc3c62493b030
Afjal13 <afjal.hossain12496@gmail.com>
2023-07-28 22:36:45 +0600

    07-28-2023

---
+++ C# Logical Program Exercise/7) Reverse Program/ReverseString/ReverseString/Program.cs
@@ -0,0 +1,25 @@
+﻿//How to reverse a string in C#?
+//declare required variable fields
+int i;
+string inputStr, reverseStr;
+//initialize by empty value
+reverseStr = string.Empty;
+
+//Take input String 
+Console.Write("\nEnter a string: ");
+inputStr = Console.ReadLine();
+//String reverse mechanism 
+for(i = inputStr.Length-1; i >=0 ; i--)
+{
+    reverseStr+=inputStr[i];
+}
+//Display results
+Console.Write("\nReverse String: ");
+Console.Write(reverseStr);
+//hold console window
+Console.ReadKey();
+//output
+//Enter a string: Afjal
+
+//Reverse String: lajfA
+
