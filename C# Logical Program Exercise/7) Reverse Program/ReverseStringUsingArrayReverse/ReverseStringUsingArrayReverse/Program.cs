0216cd8c4e3ddb845975dd011f2f0be97b25d36a
Afjal13 <afjal.hossain12496@gmail.com>
2023-07-29 01:05:41 +0600

    07-28-2023

---
+++ C# Logical Program Exercise/7) Reverse Program/ReverseStringUsingArrayReverse/ReverseStringUsingArrayReverse/Program.cs
@@ -0,0 +1,29 @@
+﻿//How to reverse a string using Array.Reverse() method in C#
+
+//declare required variable fields
+string inputStr, reverseStr;
+
+//Take input String 
+Console.Write("\nEnter a String: ");
+inputStr = Console.ReadLine();
+
+//convert to character array of given string
+char[] strArray = inputStr.ToCharArray();
+
+//Reverse of character array
+Array.Reverse(strArray);
+
+//convert to string of reverse array
+reverseStr = new string(strArray);
+
+//display reverse string
+Console.WriteLine($"\n\tThe Reverse string is: {reverseStr}");
+
+//hold console window
+Console.ReadKey();
+
+//output:
+
+//Enter a String: Afjal
+
+//        The Reverse string is: lajfA
