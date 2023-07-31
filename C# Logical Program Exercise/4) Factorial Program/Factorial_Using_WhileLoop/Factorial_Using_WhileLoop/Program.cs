f6aaa39de21293c5a4bed66529762561c3a8d13a
Afjal13 <afjal.hossain12496@gmail.com>
2023-07-31 22:04:32 +0600

    07-31-2023

---
+++ C# Logical Program Exercise/4) Factorial Program/Factorial_Using_WhileLoop/Factorial_Using_WhileLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿//Factorials of a number using while loop in C#
+//declare required variable 
+int readNumber,factNumber;
+//Read a number from console
+Console.Write("\nEnter a Number: ");
+readNumber = int.Parse(Console.ReadLine());
+//initialize by factorial start value 1
+factNumber = 1;
+//factorial mechanism
+while (readNumber != 1)
+{
+    factNumber *= readNumber;
+    readNumber--;
+}
+//display factorial output
+Console.Write($"\n\n\tFactorial is: {factNumber}");
+//hold console window
+Console.ReadLine();
+
+
+//output:
+
+//Enter a Number: 6
+
+
+//        Factorial is: 720
