0f99cc2518c0065247117101055619d690012d87
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-01 00:34:09 +0600

    07-31-2023

---
+++ C# Logical Program Exercise/4) Factorial Program/Factorial_Using_RecursiveMethod/Factorial_Using_RecursiveMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿//Factorial of a number using Recursive Method 
+//declare required variable
+int readNumber;
+long factorialNumber;
+//Read a number from console
+Console.Write("\nEnter a Number: ");
+readNumber=Convert.ToInt32(Console.ReadLine());
+// Recursive method
+factorialNumber=RecursiveFactorialMethod(readNumber);
+//display result
+Console.Write($"\n\n\tFactorial is: {factorialNumber}");
+//hold console window
+Console.ReadKey();
+//Recursive method mechanism
+static long RecursiveFactorialMethod(int readNumber)
+{
+    if(readNumber==1)
+        return 1;
+    else
+        return readNumber*RecursiveFactorialMethod(readNumber-1);
+}
+
+//output:
+
+//Enter a Number: 6
+
+
+//        Factorial is: 720
