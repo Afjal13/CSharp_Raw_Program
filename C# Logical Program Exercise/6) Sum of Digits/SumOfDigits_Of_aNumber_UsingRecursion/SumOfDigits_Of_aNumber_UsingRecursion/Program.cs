2e120c9f1561cfb800fafbebb66a401b61a300ab
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-01 23:50:53 +0600

    08-01-2023

---
+++ C# Logical Program Exercise/6) Sum of Digits/SumOfDigits_Of_aNumber_UsingRecursion/SumOfDigits_Of_aNumber_UsingRecursion/Program.cs
@@ -0,0 +1,32 @@
+﻿//Finding the sum of digits of a number using Recursion method
+//declare required field with initialize sum by 0
+int readNumber, sum = 0, temp;
+//take a number
+Console.Write("\nEnter a Number: ");
+readNumber=Convert.ToInt32(Console.ReadLine());
+//store readnumber value to temp field
+temp=readNumber;
+//calculate sum of digits value and assign the value to sum field
+sum = SumOfDigits(readNumber);
+//display sum result
+Console.Write($"\n\n\tEntered digits is: {temp}\n\n\tThe sum of digits is: {sum}");
+//hold console window
+Console.ReadKey();
+
+//recursive mechanism
+int SumOfDigits(int readNumber)
+{
+    if(readNumber ==0)
+        return 0;
+    else
+        return (readNumber%10+SumOfDigits(readNumber/10));
+}
+
+//output:
+
+//Enter a Number: 3647
+
+
+//        Entered digits is: 3647
+
+//        The sum of digits is: 20
