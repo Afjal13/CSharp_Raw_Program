94e9c598a726d1a65105acb696c08bf191725579
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-02 19:29:10 +0600

    08-02-2023

---
+++ C# Logical Program Exercise/9) Number Conversion/DecimalToBinary_UsingString/DecimalToBinary_UsingString/Program.cs
@@ -0,0 +1,26 @@
+﻿//Decimal to binary conversion using string field
+//declare variable
+int readNumber;
+string binaryResult;
+//Take a decimal number
+Console.Write("\nEnter any decimal number: ");
+readNumber=Convert.ToInt32(Console.ReadLine());
+//initialize by empty string 
+binaryResult = string.Empty;
+//decimal to binary mechanism
+for(int i = 0; readNumber > 0; i++)
+{
+    binaryResult = readNumber % 2 + binaryResult;
+    readNumber = readNumber / 2;
+}
+//display result
+Console.Write($"\n\nBinary Represenation of the given Number : {binaryResult}");
+//hold console window
+Console.ReadKey();
+
+//output:
+
+//Enter any decimal number: 10
+
+
+//Binary Represenation of the given Number : 1010
