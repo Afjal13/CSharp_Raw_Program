5bd575f1f93663bd2345d6f7c66d8d8b06bf8fc0
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-02 23:26:31 +0600

    08-02-2023

---
+++ C# Logical Program Exercise/9) Number Conversion/BinaryToDecimal_UsingLinQ/BinaryToDecimal_UsingLinQ/Program.cs
@@ -0,0 +1,18 @@
+﻿//Binary to Decimal conversion using LinQ
+//declare variable
+int binaryNumber, decimalValue;
+//Take a binary number
+Console.Write("\nEnter a binary Number: ");
+binaryNumber = int.Parse(Console.ReadLine());
+//convert mechanism using linq formula
+decimalValue = Convert.ToInt32(binaryNumber.ToString(), 2);
+//display decimal result
+Console.Write($"\n\n\tDecimal Value : {decimalValue} ");
+//hold console window
+Console.ReadKey();
+//output:
+
+//Enter a binary Number: 1010
+
+
+//        Decimal Value : 10
