3d668f082c06c2877756b42cdd44c77d194cf043
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-04 00:43:36 +0600

    08-03-2023

---
+++ C# Logical Program Exercise/11) Character Program/CharacterOccurrenceUsingLINQ/CharacterOccurrenceUsingLINQ/Program.cs
@@ -0,0 +1,13 @@
+﻿string message;
+Console.Write("\nEnter the strng: ");
+message = Console.ReadLine();
+
+Dictionary<char,int> dict =message.Replace(" ",string.Empty)
+    .GroupBy(c=>c).ToDictionary(gr=>gr.Key,gr=>gr.Count()); 
+
+foreach(var item in dict.Keys)
+{
+    Console.WriteLine(item+" : "+dict[item]);
+}
+
+Console.ReadKey();
