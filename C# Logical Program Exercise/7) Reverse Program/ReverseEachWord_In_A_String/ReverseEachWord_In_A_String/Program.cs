235fd7bddfe60cefe152e4a7224b08094ad9e353
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-06 00:37:56 +0600

    08-05-2023

---
+++ C# Logical Program Exercise/7) Reverse Program/ReverseEachWord_In_A_String/ReverseEachWord_In_A_String/Program.cs
@@ -0,0 +1,35 @@
+﻿//How to Reverse Each Word in a Given String in C#
+//declare a string field
+string inputMessage;
+//take a string message from console
+Console.Write("\nEnter the string: ");
+inputMessage = Console.ReadLine ();
+//break the given string message into sub string and also store into a string array
+string[]  messageArray = inputMessage.Split (' ');
+//declare reverse string field with initialize by empty 
+string reverseMessage = string.Empty;
+//take a temporary string field
+string temp;
+//all sub string reverse mechanism
+for (int i = 0; i < messageArray.Length; i++) //take a sub string from array
+{
+    temp = messageArray [i];
+    //reverse each sub string
+    for (int j = temp.Length-1; j>=0 ; j--) 
+    {
+        reverseMessage +=temp [j];
+    }
+    //after reverse each sub string then added white space
+    reverseMessage = reverseMessage + " ";
+}
+//display reverse string result
+Console.Write ($"\n\n\tAfter Reverse string is: {reverseMessage}");
+//hold console window
+Console.ReadKey ();
+
+//output:
+
+//Enter the string: Committed! We will save our country in any situation
+
+
+//        After Reverse string is: !dettimmoC eW lliw evas ruo yrtnuoc ni yna noitautis
