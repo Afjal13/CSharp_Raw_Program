9f391987d50083fb9edb7a2ad8fbd92c8acc5557
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-06 00:46:05 +0600

    08-05-2023

---
+++ C# Logical Program Exercise/7) Reverse Program/ReverseEachWordInStringUsingLINQ/ReverseEachWordInStringUsingLINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿//Using Linq to Reverse Each Word in C#
+Console.Write("Enter a String : ");
+string originalString = Console.ReadLine();
+
+string reverseWordString = string.Join(" ", originalString
+.Split(' ')
+.Select(x => new String(x.Reverse().ToArray())));
+Console.WriteLine($"Reverse Word String : {reverseWordString}");
+Console.ReadKey();
+//output:
+///Enter a String : Welcome to CSharp
+//Reverse Word String : emocleW ot prahSC
