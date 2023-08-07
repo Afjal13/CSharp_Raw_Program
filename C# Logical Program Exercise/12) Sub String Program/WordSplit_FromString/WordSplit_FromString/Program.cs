862263c356c68eac88fab7ebcc4f1392d4fad07f
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-07 21:39:55 +0600

    08-07-2023

---
+++ C# Logical Program Exercise/12) Sub String Program/WordSplit_FromString/WordSplit_FromString/Program.cs
@@ -0,0 +1,18 @@
+﻿//Split all word from a string
+//define a string 
+string str1 = "Hello! Bangladesh in the World.";
+//split string
+string[] str2 = str1.Split(' ');
+//display all split word
+for (int i = 0; i < str2.Length; i++)
+{
+    Console.WriteLine(str2[i]);
+}
+//hold console window
+Console.ReadKey();
+//output:
+//Hello!
+//Bangladesh
+//in
+//the
+//World.
