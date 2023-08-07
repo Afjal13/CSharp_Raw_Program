d094f610680b1c41d1b8804084768af9513641dd
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-08 00:24:26 +0600

    08-07-2023

---
+++ Basics Program/NullableType/NullableType/Program.cs
@@ -0,0 +1,26 @@
+﻿//Nullable Types in C#
+//declare three fields
+int? firstNumber=null;
+int secondNumber;
+int? lastNumber = 150;
+//Formula-1:
+if (lastNumber == null)
+    secondNumber = 0;
+else
+    secondNumber = (int)lastNumber; //or secondNumber = lastNumber.Value;
+Console.WriteLine(secondNumber);
+//Formula-2:
+secondNumber = firstNumber ?? 0;//if firstnumber is null then 0 assign into secondnumber
+Console.WriteLine(secondNumber);
+secondNumber = lastNumber ?? 50; //if lastnumber null then 50 assign to secondnumber otherwise lastnumber assign into secondnumber 
+Console.WriteLine(secondNumber);
+//Formula-3:
+secondNumber = (firstNumber == lastNumber) ? 50 : 100; //if condition true then 50 assign into otherwise 100 assign into secondnumber
+Console.WriteLine(secondNumber);
+//hold console window
+Console.ReadKey();
+//output:
+//150
+//0
+//150
+//100
