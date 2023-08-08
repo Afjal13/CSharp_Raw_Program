297735bbc1a64d0869faa9dd471701ec9be1e18f
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-08 23:38:48 +0600

    08-08-2023

---
+++ C# Logical Program Exercise/12) Sub String Program/PossibleSubStringFromAString/PossibleSubStringFromAString/Program.cs
@@ -0,0 +1,26 @@
+﻿//print possible all sub string from a string using a simple approach
+//define required library package
+using System.Text;
+//declare inputString field
+string inputString;
+//take a string
+Console.Write("\nEnter a String: ");
+inputString = Console.ReadLine();   
+//sub string mechanism
+for(int i = 0; i < inputString.Length; ++i)     //outter loop for each letter
+{
+    StringBuilder subString = new StringBuilder();    //create new string object for each sub string
+    for(int j = i; j < inputString.Length; ++j)     //inner loop for sub string on outter loop letter
+    {
+        subString.Append(inputString[j]);     //make sub string
+        Console.Write(subString + " ");    //print sub string
+    }
+    
+}
+//hold console window
+Console.ReadKey();
+
+//output:
+
+//Enter a String: xyz
+//x xy xyz y yz z
