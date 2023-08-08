4fd615a24e55978fc3474b5ae32e5277985d634a
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-09 00:04:02 +0600

    08-08-2023

---
+++ C# Logical Program Exercise/12) Sub String Program/UsingSubStrMethodFindSubStr/UsingSubStrMethodFindSubStr/Program.cs
@@ -0,0 +1,23 @@
+﻿string inputString;
+int Len;
+Console.Write("\nEnter The String: ");
+inputString = Console.ReadLine();
+Len = inputString.Length;
+
+Console.WriteLine("\n\nAll Possible Sub String are: ");
+for (int i = 0; i < Len; i++)
+{
+    for (int j = 0; j < Len - i; j++)
+    {
+        Console.Write(inputString.Substring(i, j + 1) + " ");
+    }
+}
+
+Console.ReadKey();
+
+//output:
+//Enter The String: ABCD
+
+
+//All Possible Sub String are:
+//A AB ABC ABCD B BC BCD C CD D
