219366938bcf40b5c01c4dde9eb3ac8251b84842
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-14 22:28:51 +0600

    08-14-2023

---
+++ Advanced C# Program/Practics Advanced C# Program/1) Array Program Exercises/ArrayDemo2/ArrayDemo2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Creating an Integer Array with size 3
+int[] Numbers = new int[3];
+//Accessing the Array Elements Before Initialization
+Console.WriteLine("Accessing the Array Elements Before Initialization");
+for (int i = 0; i <= Numbers.Length - 1; i++)
+{
+    Console.WriteLine($"Numbers[{i}] = {Numbers[i]}");
+}
+//Initializing the Array Elements using the Index Position
+Numbers[0] = 10;
+Numbers[1] = 20;
+Numbers[2] = 30;
+//Accessing the Array Elements After Initialization
+Console.WriteLine("\nAccessing the Array Elements After Initialization");
+for (int i = 0; i <= Numbers.Length - 1; i++)
+{
+    Console.WriteLine($"Numbers[{i}] = {Numbers[i]}");
+}
+
+Console.ReadKey();
---
+++ Advanced C# Program/Practics Advanced C# Program/1) Array Program Exercises/ArrayDemo3/ArrayDemo3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Dynamically Initializing an Array in C#
+int[] Numbers = new int[5];
+
+//before initializing the array
+Console.WriteLine("\nBefore initializing the Array: ");
+int i = 0;
+foreach(int number in Numbers)
+    Console.WriteLine($"Numbers[{i++}] = {number}");
+
+int value = 0;
+for(int j = 0; j <= Numbers.Length - 1; j++)
+{
+    value += 10;
+    Numbers[j] = value;
+}
+Console.WriteLine("\nAfter initializing the Array: ");
+//after initializing the array
+i = 0;
+foreach (int number in Numbers)
+    Console.WriteLine($"Numbers[{i++}] = {number}");
+
+Console.ReadKey();
