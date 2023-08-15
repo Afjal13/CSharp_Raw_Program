efce60438d043d7717c52bcabc6df69063cfde92
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-15 13:16:56 +0600

    08-15-2023

---
+++ Advanced C# Program/Collections in C#/Array Programs/PassingArrayAsArgs/PassingArrayAsArgs/Program.cs
@@ -0,0 +1,22 @@
+﻿//Create an array within some value
+int[] Balance = { 1000, 2, 3, 17, 50 };
+//declare required average field
+double average;
+//passing array as an method arguments and assign the average value
+average = GetAverage(Balance);
+//print the result
+Console.WriteLine($"\nAverage value is: {average}");
+//average mechanism method
+static double GetAverage(int[] Balance) //received array as an arguments in method
+{
+    double average;
+    int sum = 0;
+    for (int i = 0; i <= Balance.Length - 1; i++)
+        sum += Balance[i];
+    average = (double)sum / Balance.Length;
+    return average;
+}
+//hold console window
+Console.ReadKey();
+//output
+//Average value is: 214.4
