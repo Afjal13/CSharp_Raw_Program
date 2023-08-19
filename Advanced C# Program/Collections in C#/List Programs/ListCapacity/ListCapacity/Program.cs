6a2029f369e42d119f99fee30491abd2cf23332a
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-20 00:47:55 +0600

    08-19-2023

---
+++ Advanced C# Program/Collections in C#/List Programs/ListCapacity/ListCapacity/Program.cs
@@ -0,0 +1,34 @@
+﻿// Create a list of strings  
+List<string> CountryList = new List<string>();
+// Default Capacity  
+Console.WriteLine($"Default Capacity: {CountryList.Capacity}");
+CountryList.Add("Bangladesh");
+CountryList.Add("India");
+CountryList.Add("Rassia");
+CountryList.Add("China");
+// Original Capacity  
+Console.WriteLine($"Original Capacity: {CountryList.Capacity}");
+CountryList.Add("Iran");
+
+// Current Capacity  
+Console.WriteLine($"Current Capacity: {CountryList.Capacity}");
+
+// Trim excess  
+CountryList.TrimExcess();
+Console.WriteLine($"Trimmed Capacity: {CountryList.Capacity}");
+
+// Update Capacity  
+CountryList.Capacity = 15;
+Console.Write($"\n\t\tResize Capacity: { CountryList.Capacity}");
+Console.WriteLine($"\n\nUpdated Capacity: {CountryList.Capacity}");
+Console.ReadKey();
+
+//output:
+//Default Capacity: 0
+//Original Capacity: 4
+//Current Capacity: 8
+//Trimmed Capacity: 5
+
+//                Resize Capacity: 15
+
+//Updated Capacity: 15
---
+++ Advanced C# Program/Collections in C#/List Programs/ListCapacityImpact/ListCapacityImpact/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+List<int> List1 = new List<int>(1000); //Capacity 1000
+List<int> List2 = new List<int>(); //Default Capacity 0
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+
+for (int i = 0; i < 10000; i++)
+    List1.Add(i);
+
+stopwatch.Stop();
+Console.WriteLine($"Time Taken for List1: {stopwatch.ElapsedMilliseconds} ms.");
+
+
+stopwatch.Restart();
+
+for (int i = 0; i < 10000; i++)
+    List2.Add(i);
+
+stopwatch.Stop();
+Console.WriteLine($"\nTime Taken for List2: {stopwatch.ElapsedMilliseconds} ms.");
+
+Console.ReadKey();
+//This code will add 10000 integers to both lists and measure the time taken to do so.
+//The output will look something like this:
+
+//Time Taken for List1: 3 ms.
+
+//Time Taken for List2: 0 ms.
+
+//Which had an initial capacity of 1000, was able to add all 10000 integers without having to
+//resize its internal array.On the other hand, list2 had to resize its internal array
+//multiple times during the process, which resulted in slower performance.
+
+//This example shows that initializing a list with an appropriate capacity can improve
+//performance by reducing the number of times the list needs to resize its internal array.
+
