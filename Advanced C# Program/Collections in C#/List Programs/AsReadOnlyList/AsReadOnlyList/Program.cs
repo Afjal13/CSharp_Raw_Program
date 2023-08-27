6cd7619fad2b3124b3bc5a393b417a09692f5148
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-28 00:44:42 +0600

    08-27-2023

---
+++ Advanced C# Program/Collections in C#/List Programs/AsReadOnlyList/AsReadOnlyList/Program.cs
@@ -0,0 +1,25 @@
+﻿List<int> Numbers = new List<int> { 1,2,3,4,5};
+
+var ReadOnlyList = Numbers.AsReadOnly();
+
+Console.Write("ReadOnly List is: ");
+foreach (var element in ReadOnlyList)
+    Console.Write(element +" ");
+//ReadOnlyList.Add(10); //get error because this is only read not write in this list
+
+
+Numbers.Add(6);
+Numbers.Add(7);
+Console.WriteLine("\n");
+Console.Write("Numbers List is: ");
+foreach (var element in ReadOnlyList)
+    Console.Write(element + " ");
+
+
+
+Console.ReadKey();
+
+//output:
+//ReadOnly List is: 1 2 3 4 5
+
+//Numbers List is: 1 2 3 4 5 6 7
