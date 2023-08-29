7f09d6e1747972de8faf817b9003e43b8edcdd66
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-29 21:50:10 +0600

    08-29-2023

---
+++ Advanced C# Program/Collections in C#/List Programs/ListAnyPropertyUsingLINQ/ListAnyPropertyUsingLINQ/Program.cs
@@ -0,0 +1,37 @@
+﻿List<int> Numbers = new List<int>();
+
+var isAnyIsTrue = Numbers.Any();
+
+Console.WriteLine($"In Empty List, Any result is: {isAnyIsTrue}");
+
+Numbers.Add(1);
+Numbers.Add(2);
+Numbers.Add(3);
+Numbers.Add(4);
+Numbers.Add(5);
+
+var isAnyTrue =Numbers.Any(number=>number<2);
+Console.WriteLine($"In List, Any result is: {isAnyTrue}");
+
+//For better understanding, look at behind process of the any property in list
+bool? isAnyConditionIsTrue = null;
+foreach(var number in Numbers)
+{
+    if (number < 2)
+    {
+        isAnyConditionIsTrue=true;
+        break;
+    }
+    else
+    {
+        isAnyConditionIsTrue=false;
+    }
+}
+Console.WriteLine($"In List using foreach loop, Any result is: {isAnyConditionIsTrue}");
+
+Console.ReadKey();
+
+//output:
+//In Empty List, Any result is: False
+//In List, Any result is: True
+//In List using foreach loop, Any result is: True
