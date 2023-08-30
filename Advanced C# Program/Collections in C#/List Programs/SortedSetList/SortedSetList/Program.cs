4c77eed47e421ca2618b01f31839f05b150a0b1b
Afjal13 <afjal.hossain12496@gmail.com>
2023-08-30 21:04:04 +0600

    08-30-2023

---
+++ Advanced C# Program/Collections in C#/List Programs/SortedSetList/SortedSetList/Program.cs
@@ -0,0 +1,34 @@
+﻿//Sort List value using sort() method 
+List<int> Numbers = new List<int>();
+
+Numbers.Add(5);
+Numbers.Add(2);
+Numbers.Add(4);
+Numbers.Add(3);
+Numbers.Add(1);
+
+Numbers.Sort();
+Console.Write("using List Sort() method List is: ");
+foreach (int number in Numbers)
+    Console.Write(number + " ");
+
+Console.WriteLine("\n\n");
+//Sort List value without sort() method using sortedSet list 
+var MyNumbersList = new SortedSet<int>();
+MyNumbersList.Add(5);
+MyNumbersList.Add(2);
+MyNumbersList.Add(4);
+MyNumbersList.Add(3);
+MyNumbersList.Add(1);
+
+Console.Write("using  SortedSet List Result is: ");
+foreach (int number in MyNumbersList)
+    Console.Write(number + " ");
+
+Console.ReadKey();
+
+//output:
+//using List Sort() method List is: 1 2 3 4 5
+
+
+//using SortedSet List Result is: 1 2 3 4 5
