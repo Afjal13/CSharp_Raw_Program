2486dfb945e1cd7e5f2c0b888e32112a27f2773a
Afjal13 <afjal.hossain12496@gmail.com>
2023-09-19 23:15:59 +0600

    09-19-2023

---
+++ Machine Learning Basic/PythonASP/PythonASP/Controllers/PythonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PythonASP.Controllers
+{
+    public class PythonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
