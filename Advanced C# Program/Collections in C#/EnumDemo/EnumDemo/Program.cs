f87212a78127dbd58a5aaff1049406ba5d09a6fb
Afjal13 <afjal.hossain12496@gmail.com>
2023-09-24 23:30:02 +0600

    updated092423

---
+++ Advanced C# Program/Collections in C#/EnumDemo/EnumDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿public enum Season
+{
+    Spring,
+    Summer,
+    Autumn,
+    Winter
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Season a = Season.Autumn;
+        Console.WriteLine($"Integral value of {a} is {(int)a}");  // output: Integral value of Autumn is 2
+
+        var b = (Season)1;
+        Console.WriteLine(b);  // output: Summer
+
+        var c = (Season)4;
+        Console.WriteLine(c);  // output: 4
+    }
+}
