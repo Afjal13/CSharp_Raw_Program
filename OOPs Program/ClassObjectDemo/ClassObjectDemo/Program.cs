2995c35d720e3bd7440bf39268a4a01ffb52fae0
Afjal13 <afjal.hossain12496@gmail.com>
2023-10-08 22:46:56 +0600

    10-08-2023

---
+++ OOPs Program/ClassObjectDemo/ClassObjectDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ClassObjectDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test obj = new Test();
+            obj.num1 = 10;
+            obj.num2 = 20;
+            obj.Display();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Test
+    {
+        public int num1;
+        public int num2;
+
+        public void Display() {
+            Console.WriteLine($"Sum is: {num1+num2}");
+        }
+    }
+}
