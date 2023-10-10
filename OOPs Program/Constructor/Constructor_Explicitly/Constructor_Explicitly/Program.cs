3b3d71f219cbfbced646a4a6987fefe9adbe6dbc
Afjal13 <afjal.hossain12496@gmail.com>
2023-10-10 23:40:47 +0600

    10-10-2023

---
+++ OOPs Program/Constructor/Constructor_Explicitly/Constructor_Explicitly/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml;
+
+namespace Constructor_Explicitly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            First f1 = new First();
+            First f2 = new First();
+            First f3 = new First();
+            Console.WriteLine($"{f1.x}   {f2.x}   {f3.x}");
+            Second s1 = new Second(100); //100 wll send to local variable x
+            Second s2 = new Second(200); //200 wll send to local variable x
+            Second s3 = new Second(300); //300 wll send to local variable x
+            Console.WriteLine($"{s1.x}   {s2.x}   {s3.x}");
+            Console.ReadKey();
+        }
+    }
+    class First
+    {
+        public int x = 100;
+    }
+
+    class Second
+    {
+        public int x;
+        //Parameterized Explicit Constructor
+        public Second(int x)
+        {
+            this.x = x;
+        }
+    }
+}
+
+//Output:
+//100   100   100
+//100   200   300
---
+++ OOPs Program/Constructor/Parameterized_Constructor/Parameterized_Constructor/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Parameterized_Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee(101, 30, "Hussain", "Dhaka", true);
+            e1.Display();
+            Console.WriteLine();
+            Employee e2 = new Employee(101, 28, "Sazzad", "Comilla", false);
+            e2.Display();
+            Console.ReadKey();
+        }
+    }
+
+    class Employee
+    {
+        public int Id, Age;
+        public string Address, Name;
+        public bool IsPermanent;
+        //User Defined Parameterized Constructor
+        public Employee(int id, int age, string name, string address, bool isPermanent)
+        {
+            Id = id;
+            Age = age;
+            Address = address;
+            Name = name;
+            IsPermanent = isPermanent;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Employee Id is:  " + Id);
+            Console.WriteLine("Employee Name is:  " + Age);
+            Console.WriteLine("Employee Address is:  " + Address);
+            Console.WriteLine("Employee Name is:  " + Name);
+            Console.WriteLine("Is Employee Permanent:  " + IsPermanent);
+        }
+    }
+}
