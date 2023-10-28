7b47fd9b4d88f92679d8e9b67b5c743eca66e03f
Afjal13 <afjal.hossain12496@gmail.com>
2023-10-28 23:55:16 +0600

    10-28-2023

---
+++ C# Logical Program Exercise/13) Recursive/RecursiveDemo/RecursiveDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RecursiveDemo
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+           Program program = new Program();
+            int[] y = { 1, 3, 2, 8 };
+            program.function(y, 2);
+
+            Console.ReadKey();  
+
+        }
+        int function(int[] x, int i)
+        {
+            int s = x[i];
+            if (i > 0)
+                s =s+ function(x, i - 1);
+            Console.WriteLine(s);
+            return s;
+        }
+    }
+}
