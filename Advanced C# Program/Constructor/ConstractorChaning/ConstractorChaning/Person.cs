b6ed1132bf216523c4a8c9613fadd07c247e04ef
Afjal13 <afjal.hossain12496@gmail.com>
2024-03-04 23:02:42 +0600

    Update 03-04-2024

---
+++ Advanced C# Program/Constructor/ConstractorChaning/ConstractorChaning/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstractorChaning
+{
+    public class Person
+    {
+        private string? firstName;
+        private string? middleName;
+        private string? lastName;
+
+
+        public Person(string firstName, string middleName, string lastName):this(firstName,lastName)
+        {
+            this.middleName = middleName;
+        }
+        public Person(string firstName, string lastName):this(firstName)
+        {
+            this.lastName = lastName;
+        }
+
+        public Person(string firstName):this()
+        {
+            this.firstName = firstName;
+        }
+
+        public Person() { }
+
+
+
+        public string GetName()
+        {
+            return firstName + " " + middleName + " " + lastName;
+        }
+    }
+}
---
+++ Advanced C# Program/Constructor/ConstractorChaning/ConstractorChaning/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConstractorChaning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.WriteLine($"Person Name: {person.GetName()}");
+
+            Person person1 = new Person("Afjal");
+            Console.WriteLine($"Person1 Name: {person1.GetName()}");
+
+            Person person2 = new Person("Afjal", "Hossain");
+            Console.WriteLine($"Person2 Name: {person2.GetName()}");
+
+            Person person3 = new Person("Afjal","Hossain","Monni");
+            Console.WriteLine($"Person3 Name: {person3.GetName()}");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
